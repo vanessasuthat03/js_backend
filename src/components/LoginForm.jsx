d3410833ac9583938739ab18ac06f9a222f06e20
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-09-10 13:56:58 +0200

    * Create loginform. * Move fetch login to loginform.

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
+const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`
+// const EVENT_LIST_URL =
+
+// STYLE
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("test.user@willandskill.se")
+  const [password, setPassword] = useState("js-lesson-10")
+  const [token, setToken] = useState(null)
+
+  function login() {
+    console.log(email, password)
+    const payload = {
+      email: email,
+      password: password
+    }
+
+    fetch(LOGIN_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(res => res.json()) // det man får tillbaka från fetch
+      .then(data => {
+        console.log("data", data.token) // får fram token
+        setToken(data.token)
+      })
+  }
+
+  return (
+    <div>
+      <label htmlFor="email">Email</label>
+      <input
+        name="email"
+        type="email"
+        value={email}
+        onChange={event => setEmail(event.currentTarget.value)}
+        placeholder="user@hotmail.com"
+      />
+      <div></div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={event => setPassword(event.currentTarget.value)}
+        />
+      </div>
+      <button onClick={login}>Logga in</button>
+    </div>
+  )
+}
