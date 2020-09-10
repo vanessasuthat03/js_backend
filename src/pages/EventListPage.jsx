afb39cf43d1924336301c4d5d57f0179502150d5
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-09-10 15:28:02 +0200

    Set response to usestate and render eventlist.

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { UserContext } from "../contexts/UserContext"
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
@@ -6,7 +6,9 @@ const EVENT_LIST_URL = `${ROOT_URL}events/events/`
 
 export default function EventListPage() {
   const { token } = useContext(UserContext)
+  const [eventList, setEventList] = useState(null)
 
+  // Bör vara så lång upp som möjligt
   useEffect(() => {
     fetchEventList()
   }, [])
@@ -18,11 +20,26 @@ export default function EventListPage() {
         Authorization: `Bearer ${token}`
       }
     })
+      .then(res => res.json())
+      .then(data => {
+        console.log("data.results", data.results)
+        setEventList(data.results)
+      })
+      .catch(error => {
+        console.log("error", error)
+      })
   }
   return (
     <div>
       <h1>Event list page</h1>
-      {token}
+      {eventList &&
+        eventList.map(eventItem => {
+          return (
+            <div key={eventItem.id}>
+              <p>{eventItem.title}</p>
+            </div>
+          )
+        })}
     </div>
   )
 }
