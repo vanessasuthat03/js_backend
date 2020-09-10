c62e5a280949fa2f7a85317ec905975adff44d09
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-09-10 15:30:25 +0200

    Add Link to eventlistpage.

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import { UserContext } from "../contexts/UserContext"
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
@@ -36,7 +37,7 @@ export default function EventListPage() {
         eventList.map(eventItem => {
           return (
             <div key={eventItem.id}>
-              <p>{eventItem.title}</p>
+              <Link to={`event/${eventItem.slug}`}>{eventItem.title}</Link>
             </div>
           )
         })}
