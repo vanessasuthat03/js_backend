164731e6aba48f11a98710549136f516ede899ed
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-09-10 16:03:45 +0200

    Add EventDetalPage in route path.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import EventListPage from "./pages/EventListPage"
 import { UserContext } from "./contexts/UserContext"
+import EventDetailPage from "./pages/EventDetailPage"
 
 function App() {
   const [token, setToken] = useState(null)
@@ -11,10 +12,11 @@ function App() {
     <div className="App">
       <UserContext.Provider value={{ token, setToken }}>
         <Switch>
+          <Route path="/event/:slug" component={EventDetailPage} />{" "}
+          {/*kolon innan slug säger till att leta efter slug.*/}
           <Route path="/event-list">
             <EventListPage />
           </Route>
-
           <Route path="/">
             <LoginPage />
           </Route>
